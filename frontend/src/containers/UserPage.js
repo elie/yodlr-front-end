8e1ec15e35d3540f6f41b444207b2f913ccbef96
Elie Schoppik <eschoppik@gmail.com>
2018-06-14 15:28:50 -0700

    users can register, data being saved on backend

--- frontend/src/containers/UserPage.js
+++ frontend/src/containers/UserPage.js
@@ -1,7 +1,78 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class UserPage extends Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    hasRegistered: false
+  };
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    let { firstName, lastName, email } = this.state;
+    e.preventDefault();
+    axios
+      .post("http://localhost:3000/users", {
+        firstName,
+        lastName,
+        email
+      })
+      .then(() => {
+        this.setState({
+          hasRegistered: true
+        });
+      })
+      .catch(() => {
+        alert("something went wrong!");
+      });
+  };
   render() {
-    return <h1>Hello from UserPage!</h1>;
+    if (!this.state.hasRegistered) {
+      return (
+        <div>
+          <h1>Welcome!</h1>
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              <label htmlFor="firstName">First Name:</label>
+              <input
+                type="text"
+                name="firstName"
+                id="firstName"
+                value={this.state.firstName}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="lastName">Last Name:</label>
+              <input
+                type="text"
+                name="lastName"
+                id="lastName"
+                value={this.state.lastName}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                onChange={this.handleChange}
+                value={this.state.email}
+              />
+            </div>
+            <button>Register!</button>
+          </form>
+        </div>
+      );
+    } else {
+      return <h2>Thank you for signing up!</h2>;
+    }
   }
 }
